3e867210693f420005f52637ba0f81e291005ede
Gabby Getz <gabby@cesium.com>
2023-06-07 14:34:50 -0400

    install deps before npm install

--- travis/test-release.sh
+++ travis/test-release.sh
@@ -3,6 +3,5 @@ set -ev
 if [ $TRAVIS_BRANCH != "cesium.com" ]; then
   npm --silent run build-release
   npm --silent run test -- --browsers ChromeCI --failTaskOnError --webgl-stub --release --suppressPassed
-  npx playwright install-deps
   npm --silent run test-e2e
 fi
